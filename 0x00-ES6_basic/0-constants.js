f44bf9b1774b4266bfbda2d4ab8054cdb8268410
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-04-28 23:16:29 +0300

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,11 +2,11 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-
+/* eslint export function  getLast*/
 export function getLast() {
   return ' is okay';
 }
-
+/* eslint export function taskNext */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
